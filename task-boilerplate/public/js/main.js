db6334a55616d33aa75c689c3c06dc904de07159
mathilde <mathilde.orstavik@norkart.no>
2017-09-14 12:13:41 +0200

    updated boilerplate to new challenge: glos survival guide

--- task-boilerplate/public/js/main.js
+++ task-boilerplate/public/js/main.js
@@ -4,7 +4,7 @@ function setMap() {
 
       console.log('Loading map');
 
-      var map = L.map('map').setView([63.430, 10.395], 14);
+      var map = L.map('map').setView([63.41737, 10.40417], 15.5);
       //Set view takes two parameters;
             //1. The coordinates for the center of the map
             //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
@@ -15,7 +15,7 @@ function setMap() {
       L.tileLayer(basemapUrl).addTo(map);
 
       //Adding geoJSON layer to the map:
-      L.geoJSON(utesteder)
+      L.geoJSON(classrooms)
       .addTo(map);
 }
 
